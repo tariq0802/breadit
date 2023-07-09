27f1f3a3bf6d93b9ba6f19ffef9600366653f1cf
Tariq Ahamad <tariqahamad@Tariqs-MacBook-Air.local>
2023-07-09 15:00:31 +0530

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
+      <h1 className="font-bold text-3xl md:text-4xl">Your feed Tariq Ahmad</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* @ts-expect-error server component */}
         {session ? <CustomFeed /> : <GeneralFeed />}
